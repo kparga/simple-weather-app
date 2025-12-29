ff5466c65592a8761445eb4ee7ba429d921194fe
Karina Parga Valenzuela <karinapargavalenzuela@Karinas-MacBook-Pro.local>
2025-12-28 23:16:20 -0300

    ADD footer test

--- __tests__/LastUpdatedFooter.test.tsx
+++ __tests__/LastUpdatedFooter.test.tsx
@@ -1,3 +1,12 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import { LastUpdatedFooter } from "@/app/components/LastUpdatedFooter";
+
+describe("LastUpdatedFooter Component test", () => {
+  it("renders a date and time ", async () => {
+    const fechaEnMiliseg = new Date(Date.now()).toString();
+    render(<LastUpdatedFooter/>);
+    const children = screen.getByText('Last Updated on '+ fechaEnMiliseg)
+    expect(children).toBeInTheDocument()
+  });
+});
